579d2153b00bd79c4e9bd7ab15ce18146a658fb4
yalmaw <yaalmaw@gmail.com>
2023-07-08 11:05:06 -0400

    Add project files.

---
+++ HRM.API/Controllers/EmployeesController.cs
@@ -0,0 +1,35 @@
+﻿using HRM.API.Models;
+using HRM.API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRM.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeesController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+
+
+    }
+        [HttpGet("[action]")]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        {
+            try
+            {
+                var allemployess = await _employeeService.GetEmployees();
+                return Ok(allemployess);
+            }
+            catch (Exception exp)
+            {
+                return BadRequest(exp.Message);
+            }
+            //return await _context.Employees.ToListAsync();
+        }
+    }
+}
