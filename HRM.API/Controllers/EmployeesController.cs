c3a1170a83fca11378799a5a50b1e7904e4bf785
yalmaw <yaalmaw@gmail.com>
2023-07-08 11:44:20 -0400

    This is for Jenkins demo

--- HRM.API/Controllers/EmployeesController.cs
+++ HRM.API/Controllers/EmployeesController.cs
@@ -31,5 +31,64 @@ namespace HRM.API.Controllers
             }
             //return await _context.Employees.ToListAsync();
         }
+        [HttpGet("Employee/{id}")]
+        public async Task<ActionResult<Employee>> GetEmployeeById(int id)
+        {
+            try
+            {
+                var employee = await _employeeService.GetEmployee(id);
+                return Ok(employee);
+            }
+            catch (Exception exp)
+            {
+                return BadRequest(exp.Message);
+            }
+        }
+        [HttpPost("AddEmployee")]
+        public async Task<ActionResult> AddEmployee([FromBody] Employee employee)
+        {
+            try
+            {
+                string result = string.Empty;
+                if (employee != null)
+                {
+                    result = await _employeeService.AddEmployee(employee);
+                }
+                return Ok(result);
+            }
+            catch (Exception exp)
+            {
+                return BadRequest(exp.Message);
+                //
+            }
+        }
+        [HttpPut("EditEmployee/{id}")]
+        public async Task<ActionResult> EditEmployee(int id, [FromBody] Employee employee)
+        {
+            try
+            {
+                string result = string.Empty;
+                result = await _employeeService.EditEmployee(id, employee);
+                return Ok(result);
+            }
+            catch (Exception exp)
+            {
+                return BadRequest(exp.Message);
+            }
+        }
+        [HttpDelete("DeleteEmployee/{id}")]
+        public async Task<ActionResult> DeleteEmployee(int id)
+        {
+            try
+            {
+                string result = string.Empty;
+                result = await _employeeService.RemoveEmployee(id);
+                return Ok(result);
+            }
+            catch (Exception exp)
+            {
+                return BadRequest(exp.Message);
+            }
+        }
     }
 }
