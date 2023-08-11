8aac5169f5d11b9bbccc7963aa2e825a5f4f873e
yalmaw <yaalmaw@gmail.com>
2023-08-11 11:13:15 -0400

    To Test Jenkins Trigger when cmmit and push changes

--- HRM.API/Models/Employee.cs
+++ HRM.API/Models/Employee.cs
@@ -4,10 +4,10 @@
     {
 
         public int Id { get; set;}
-        public string Name { get; set; }
-        public string? Designation { get; set; }
-        public string? FathersName { get; set; }
-        public string ?MothersName { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public string Name { get; set;}
+        public string? Designation { get; set;}
+        public string? FathersName { get; set;}
+        public string ?MothersName { get; set;}
+        public DateTime DateOfBirth { get; set;}
     }
 }
